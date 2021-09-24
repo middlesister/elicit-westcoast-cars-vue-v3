11d4684db2ef4c12cd99e0050c52c5bd8da995d3
Karin Taliga <karin.taliga@elicit.se>
2021-09-24 14:53:35 +0200

    Copy over styles from previous project to main component

--- src/App.vue
+++ src/App.vue
@@ -23,3 +23,226 @@
     }
   } 
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap');
+
+:root {
+  --primary-color: #2196f3;
+  --secondary-color: #00796b;
+  --dark-color: #212121;
+  --light-color: #f4f4f4;
+  --background-color: #2b2a2a;
+}
+
+/* Reset */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Open Sans', sans-serif;
+  background-color: var(--background-color);
+  color: var(--light-color);
+  line-height: 1.6;
+}
+
+h1,
+h2 {
+  font-weight: 300;
+  line-height: 1.2;
+}
+
+input[type='text'] {
+  max-width: 400px;
+  min-width: 250px;
+  padding: 0.5rem;
+  /* border-radius: 5px; */
+  background-color: transparent;
+  color: var(--light-color);
+  /* border: solid 1px darkgray; */
+  border: none;
+  border-bottom: 1px solid var(--light-color);
+}
+
+input:focus {
+  outline: none;
+}
+ul {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+  color: var(--dark-color);
+}
+
+p {
+  margin: 1rem 0;
+}
+
+img {
+  width: 100%;
+}
+
+footer {
+  padding: 1.5rem;
+  text-align: center;
+}
+
+/* Classes that are shared... */
+.content {
+  padding: 5rem 2rem;
+  margin-bottom: 3rem;
+}
+
+.text-primary {
+  color: var(--primary-color);
+}
+
+.cars img:hover {
+  opacity: 0.7;
+}
+
+.heading {
+  text-align: center;
+  padding: 3rem 0;
+}
+
+.card {
+  padding: 10px;
+  margin-bottom: 20px;
+  border-radius: 10px;
+  background-color: #6b6a6a;
+  box-shadow: 0 2px 10px 1px rgba(0, 0, 0, 0.8);
+}
+
+.thumbnail {
+  width: 100px;
+}
+
+/* Icons */
+.icons {
+  padding: 2rem;
+}
+
+.icons h3 {
+  font-weight: 400;
+  margin-bottom: 1rem;
+}
+
+.icons i {
+  background-color: var(--primary-color);
+  color: var(--light-color);
+  padding: 1rem;
+  border-radius: 50%;
+  margin-bottom: 1rem;
+}
+
+.btn {
+  cursor: pointer;
+  display: inline-block;
+  padding: 0.8rem 2rem;
+  border: none;
+  border-radius: 10px;
+}
+
+.btn-primary,
+.bg-primary {
+  color: var(--light-color);
+  background-color: var(--primary-color);
+}
+
+.btn-secondary,
+.bg-secondary {
+  color: var(--light-color);
+  background-color: var(--secondary-color);
+}
+
+.btn-dark,
+.bg-dark {
+  color: var(--light-color);
+  background: var(--dark-color);
+}
+
+.btn-light,
+.bg-light {
+  color: var(--dark-color);
+  background-color: var(--light-color);
+}
+
+.btn-outline {
+  background: transparent;
+  border: solid 1px var(--light-color);
+  color: var(--light-color);
+}
+
+.btn-outline-rounded {
+  background: transparent;
+  border: solid 2px var(--light-color);
+  color: var(--light-color);
+  border-radius: 1.6rem;
+}
+
+.mb-3 {
+  margin-bottom: 0.6rem;
+}
+.mb-6 {
+  margin-bottom: 1.2rem;
+}
+
+.vehicle-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.vehicle-form-two-button {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 10px;
+}
+
+/* Navigation */
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: var(--dark-color);
+  opacity: 0.8;
+  width: 100%;
+  color: var(--light-color);
+  height: 4.5rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: 0 2rem;
+}
+
+.navbar a {
+  color: var(--light-color);
+  padding: 0.8rem 0.5rem;
+  margin: 0 3px;
+}
+
+.navbar a:hover {
+  border-bottom: solid 2px var(--primary-color);
+}
+
+.navbar ul {
+  display: flex;
+}
+
+.navbar .logo {
+  font-weight: 400;
+}
+
+.active {
+  border-bottom: solid 2px var(--primary-color);
+}
+
+
+</style>
