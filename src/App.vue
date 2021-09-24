6f465a8e9be20a38cb1e8f45d51049ccd10d3b48
Karin Taliga <karin.taliga@elicit.se>
2021-09-24 14:46:36 +0200

    Add initial template for App component

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <div class="navbar" id="navbar">
+      <h1 class="logo">
+        <span class="text-primary">
+          <i class="fas fa-car">Westcoast</i> Cars
+        </span>
+      </h1>
+      <ul>
+        <li><a href="#" class="active">Start</a></li>
+        <li><a href="#">Galleri</a></li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Välkommen till vår sajt',
+      }
+    }
+  } 
+</script>
