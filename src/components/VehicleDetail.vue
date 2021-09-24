2e4ef782b7b17512fd921d3a80d1278109886709
Karin Taliga <karin.taliga@elicit.se>
2021-09-24 15:16:07 +0200

    Add image thumbnail for VehicleDetail component

--- src/components/VehicleDetail.vue
+++ src/components/VehicleDetail.vue
@@ -1,11 +1,15 @@
 <template>
   <section class="card">
     <div class="vehicle-item">
+      <img v-bind:src="vehicle.imageUrl" alt="" class="thumbnail">
       <div>{{ vehicle.registrationNo }}</div>
       <div>{{ vehicle.make }}</div>
       <div>{{ vehicle.model }}</div>
       <div>{{ vehicle.modelYear }}</div>
-      <div>{{ vehicle.mileage }}</div>
+      <button 
+        v-on:click="onToggleMileage"
+        class="btn btn-outline-rounded">{{ displayMileage ? 'Göm' : 'Visa' }} Km</button>
+      <div v-if="displayMileage">{{ vehicle.mileage }}</div>
     </div>
   </section>
 </template>
@@ -21,12 +25,17 @@
             make: 'Chevrolet',
             model: 'Corvette',
             modelYear: '2015',
-            imageUrl: '',
+            imageUrl: 'https://i.postimg.cc/85xqHwJQ/car3.jpg',
             mileage: 15000,
             description: ''
         },
       };
     },
+    methods: {
+      onToggleMileage() {
+        this.displayMileage = !this.displayMileage;
+      },
+    }
   };
 </script>
 
